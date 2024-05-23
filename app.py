38cd5178395ae261912ebf6109a92639a7360652
Guy Marshall <guymarshall9@gmail.com>
2024-05-23 17:47:29 +0100

    UPDATE: Add todo to allow browser choice

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import time
 
 urls = [] # TODO: read from file
 
-browser = webbrowser.get('firefox')
+browser = webbrowser.get('firefox') # TODO: allow browser choice
 
 for url in urls:
     browser.open_new_tab(url)
